54ca8b070aaea839b3ea8046bb1f48925d1bcfc2
Mhmd Shami <mhmddshamii@gmail.com>
2025-08-25 13:37:19 +0300

    finishing migrations, adding enums for statuses and role, creating models

---
+++ database/migrations/2025_08_25_091922_create_transactions_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+
+            $table->foreignUuid('user_id')->constrained()->nullOnDelete();
+
+            $table->foreignUuid('wallet_id')->constrained()->nullOnDelete();
+
+            //have value ony if transaction type is P2P
+            $table->foreignUuid('related_wallet_id')->nullable()->constrained('wallets')->nullOnDelete();
+
+            $table->string('user_transaction_role')->default('sender');
+
+
+            $table->unsignedBigInteger('amount');
+            $table->string('transaction_type')->default('P2P');
+
+            $table->string('description');
+            $table->timestampTz("transaction_date_time");
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
