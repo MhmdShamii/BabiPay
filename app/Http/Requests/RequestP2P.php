bb8bac43fe5f0044c59431ff7da68943ac318257
Mhmd Shami <mhmddshamii@gmail.com>
2025-09-08 03:02:55 +0300

    P2P validation identiofier fixing now can send by email or username

--- app/Http/Requests/RequestP2P.php
+++ app/Http/Requests/RequestP2P.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use App\Models\User;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Auth;
 
@@ -24,7 +25,18 @@ class RequestP2P extends FormRequest
     {
         return [
             'sender_wallet_id' => 'required|exists:wallets,id',
-            'receiverIdentifier' => 'required|exists:users,username',
+            'receiverIdentifier' => ['required', 'receiverIdentifier' => [
+                'required',
+                function ($attribute, $value, $fail) {
+                    $exists = User::where('email', $value)
+                        ->orWhere('username', $value)
+                        ->exists();
+
+                    if (!$exists) {
+                        $fail('The specified user does not exist.');
+                    }
+                }
+            ],],
             'description' => 'nullable|string|max:255',
             'amount'    => 'required|numeric|min:0.01',
         ];
